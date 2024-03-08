91eff8407eb46228332c32e984e25ff93cf26300
Aakrist Godar <aakrist.godar.1@gmail.com>
2024-03-08 09:43:44 -0600

    shooter update

--- src/main/java/frc/robot/subsystems/ManualShooter.java
+++ src/main/java/frc/robot/subsystems/ManualShooter.java
@@ -1,10 +1,6 @@
 package frc.robot.subsystems;
 
 
-
-
-
-
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
@@ -15,7 +11,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
 /* Github testing */
-public class Shooter extends SubsystemBase{
+public class ManualShooter extends SubsystemBase{
 
 
 
@@ -28,11 +24,10 @@ public class Shooter extends SubsystemBase{
   //TODO: update the launch speed
 
 
-  private Apriltags m_Apriltags = new Apriltags();
   private PneumaticsSubsystem m_PneumaticsSubsystem = new PneumaticsSubsystem();
   private PIDController m_XPidController = new PIDController(.6, 0, 0);
   private PIDController m_YPidController = new PIDController(.6, 0, 0);
-  //private PIDController m_AreaPidController = new PIDController(.6, 0, 0);
+//    private PIDController m_AreaPidController = new PIDController(.6, 0, 0);
   private Swerve m_Swerve;
   //private double tolerance = 0.1;
   //private boolean onTarget = false;
@@ -43,7 +38,7 @@ public class Shooter extends SubsystemBase{
 
 
 
-  public Shooter(Swerve m_Swerve) {
+  public ManualShooter(Swerve m_Swerve) {
    this.m_Swerve = m_Swerve;
 
 
@@ -84,38 +79,33 @@ public class Shooter extends SubsystemBase{
   //tolerance
   m_XPidController.setTolerance(Constants.Shooter.tolerance);
   m_YPidController.setTolerance(Constants.Shooter.tolerance);
-  //m_AreaPidController.setTolerance(Constants.Shooter.areaTolerance);
+//    m_AreaPidController.setTolerance(Constants.Shooter.areaTolerance);
   }
 
 
 
-  public void off()
-  {
-     shooterCANSparkMax.set(0);
-     shooterCANSparkMaxThree.set(0);
-  }
 
-  public void cherryBomb() {
-    if(m_Apriltags.getV()!=0){
-         //TODO: Add the launch speed
-         boolean withinXTolerance = (m_Apriltags.getX()<Constants.Shooter.tolerance&&m_Apriltags.getX()>-Constants.Shooter.tolerance);
-         boolean withinYTolerance = (m_Apriltags.getY()<Constants.Shooter.tolerance&&m_Apriltags.getY()>-Constants.Shooter.tolerance);
-         // boolean withinAreaTolerance = (m_Apriltags.getArea()-Constants.Shooter.targetArea<Constants.Shooter.areaTolerance&&m_Apriltags.getArea()-Constants.Shooter.targetArea>-Constants.Shooter.areaTolerance);
-         shooterCANSparkMax.set(Constants.Shooter.launchSpeedLimit);
-         while(shooterCANSparkMax.getEncoder().getVelocity()<0.6||!Constants.Shooter.onTarget){
-              Constants.Shooter.onTarget = withinXTolerance&&withinYTolerance;//&&withinAreaTolerance
-              if(!withinXTolerance){
-                   m_Swerve.drive(0, 0, m_XPidController.calculate(m_Apriltags.getX(), 0/*TODO:Might need to change */), false);
-              }
-              if(!withinYTolerance){
-                   shooterCANSparkMaxThree.set(m_YPidController.calculate(m_Apriltags.getY(), 0));
-              }
-              // if(!withinAreaTolerance){
-              //      m_Swerve.drive(m_AreaPidController.calculate(m_Apriltags.getArea(), /* TODO: Change the setpoint */Constants.Shooter.targetArea), 0, 0, false);
-              // }
-         }
-         m_PneumaticsSubsystem.toggle();
-    }
-  }
- 
- }
+  public void shooterAngleUp(double speed){
+    shooterCANSparkMaxThree.setInverted(false);
+    shooterCANSparkMaxThree.set(speed);
+}
+
+
+public void off(){
+    shooterCANSparkMaxThree.set(0);
+}
+
+//TODO: Change 
+public void shooterAngleDown(double speed){
+    shooterCANSparkMaxThree.setInverted(true);
+    shooterCANSparkMaxThree.set(speed);
+}
+public void cherryBomb() {
+        //TODO: Add the launch speed
+        shooterCANSparkMax.set(Constants.Shooter.launchSpeedLimit);
+        while(shooterCANSparkMax.getEncoder().getVelocity()<0.6){
+        }
+        m_PneumaticsSubsystem.toggle();
+}
+
+}
